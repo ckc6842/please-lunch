cd96b882a6ea2ac1a519240206810e2d2f3c9e4a
Taehwan Kim <maxtortime@gmail.com>
2015-07-31 20:15:18 +0900

    FOOD테이블에 DB넣는 모듈. sb-admin2 템플릿사용.

--- app/mod_administrator/controllers.py
+++ app/mod_administrator/controllers.py
@@ -1,21 +1,38 @@
 # Import flask dependencies
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from app import app, db
-from app.models import Food
+from app.models import Food, Cook, Taste, Nation
 
 
-# Define the blueprint: 'auth', set its url prefix: app.url/auth
-mod_administrator = Blueprint('administrator', __name__)
+mod_administrator = Blueprint('administrator', __name__, url_prefix='/admin')
 
-# Set the route and accepted methods
-@mod_administrator.route('/admin/', methods=['GET', 'POST'])
+
+@mod_administrator.route('/food', methods=['GET', 'POST'])
 def administrator():
+    print "start"
+    return render_template("administrator/sb-admin/pages/index.html")
+
+
+@mod_administrator.route('/food/add', methods=['GET', 'POST'])
+def addfood():
     if request.method == 'POST':
         print request.get_data()
-        food = Food(request.json['foodName'])
-        db.session.add(food)
+        temp = Food(request.json['foodName'])
+        db.session.add(temp)
         db.session.commit()
-        return "GOOD"
+    return 'seccess'
 
-    food = Food.query.all()
-    return render_template("administrator/admin.html", default=food)
+@mod_administrator.route('/food/delete', methods=['GET', 'POST'])
+def deletefood():
+    if request.method == 'POST':
+        print request.get_data()
+        temp = Food(request.json['foodName'])
+        Food.query.filter_by(foodName=temp.foodName).delete()
+        db.session.commit()
+    return 'seccess'
+
+@mod_administrator.route('/food/dataget', methods=['GET'])
+def dataget():
+    data = Food.query.all()
+    return jsonify({'food': [{'foodName': item.foodName, 'id': item.id} for item in data]})
+
--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -1,11 +1,31 @@
 var app = angular.module('app', []);
 
-app.controller("subCtrl", ['$scope', '$http', function($scope, $http){
-    var users = [];
-    $scope.tempData = {};
-    $scope.tempData.users = users;
-    $scope.submitData = function(temp){
-        $http.post('http://127.0.0.1:8080/admin/', angular.fromJson({ foodName : temp }))
-        users.push(angular.fromJson({ name : temp }));
+app.controller("mainCtrl", ['$scope', '$http', function($scope, $http){
+
+    $http.get('http://127.0.0.1:8080/admin/food/dataget')
+        .success(
+        function(response){ $scope.tabledata = response.food }
+        )
+
+    $scope.submitFood = function(temp){
+
+        $http.post('http://127.0.0.1:8080/admin/food/add', angular.fromJson({ foodName : temp }));
+        $scope.formData_add_food = '';
+        $http.get('http://127.0.0.1:8080/admin/food/dataget')
+            .success(
+            function(response){ $scope.tabledata = response.food }
+            )
     };
+
+    $scope.deleteFood = function(temp){
+
+        $http.post('http://127.0.0.1:8080/admin/food/delete', angular.fromJson({ foodName : temp }));
+        $scope.formData_del_food = '';
+        $http.get('http://127.0.0.1:8080/admin/food/dataget')
+            .success(
+            function(response){ $scope.tabledata = response.food }
+            )
+    };
+
+
 }])
