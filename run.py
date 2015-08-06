31cbb56dcad915109d2c68f2edf9940c669da16b
Taehwan Kim <maxtortime@gmail.com>
2015-08-06 08:31:27 +0000

    run.py 바꿈

--- run.py
+++ run.py
@@ -2,8 +2,8 @@
 from app import app
 
 # for ec2 instance testing.
-# app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
 
 # for Windows local testing.
-app.run(host='127.0.0.1', port=80, debug=True)
+# app.run(host='127.0.0.1', port=80, debug=True)
 
