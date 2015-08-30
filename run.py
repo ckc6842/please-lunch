1bb9865904e58b3f6b51dd2a8ce8aae9f84c2ce6
maxtortime@gmail.com <maxtortime@gmail.com>
2015-08-30 09:47:11 +0900

    run.py if __name__ == 'main': 추가

--- run.py
+++ run.py
@@ -7,12 +7,10 @@ from celery import Celery
 
 # 'app.run' is different by os.
 import sys
-
-if sys.platform == 'win32':
-    app.run(host='127.0.0.1', port=8080, debug=True)
-elif sys.platform == 'linux2':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-else:
-    app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == '__main__':
+	if sys.platform == 'win32':
+	    app.run(host='127.0.0.1', port=8080, debug=True)
+	elif sys.platform == 'linux2':
+	    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
