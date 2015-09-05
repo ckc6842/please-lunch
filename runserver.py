c29b77be9b43e09e1020bafb8b84ab667b22704b
maxtortime <maxtortime@gmail.com>
2015-09-05 04:35:13 -0400

    서버 실행

--- runserver.py
+++ runserver.py
@@ -8,11 +8,4 @@ from celery import Celery
 # 'app.run' is different by os.
 import sys
 
-if sys.platform == 'win32':
-    app.run(host='127.0.0.1', port=8080, debug=True)
-elif sys.platform == 'linux2':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-else:
-    app.run(host='127.0.0.1', port=8080, debug=True)
-
-
+app.run(host='0.0.0.0', port=80)
