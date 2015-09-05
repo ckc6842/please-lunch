032f93b45972a6b11c85fd55f1a67dcf41e1ff2d
maxtortime <maxtortime@gmail.com>
2015-09-05 17:37:47 +0900

    start.js url 수정

--- app/static/js/start.js
+++ app/static/js/start.js
@@ -1,13 +1,13 @@
 angular.module('app')
     .controller('startCtrl', ['$scope','$http', '$rootScope', '$state', function($scope, $http, $rootScope, $state) {
 
-        $http.get('http://127.0.0.1:8080/start/getfoodlist')
+        $http.get('http://0.0.0.0:80/start/getfoodlist')
         .success(function(response){
             $scope.foodlist = response.foodlist;
         });
 
         $scope.sendrating = function(rating, foodName, userId){
-            $http.post('http://127.0.0.1:8080/start/getfoodlist/', angular.fromJson({userId : userId, foodName : foodName, rating : rating}))
+            $http.post('http://0.0.0.0:8080/start/getfoodlist/', angular.fromJson({userId : userId, foodName : foodName, rating : rating}))
             alert(foodName + " is " +rating +"점 " + userId);
         }
 
