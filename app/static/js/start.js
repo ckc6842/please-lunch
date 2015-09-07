4a1bbd75d4a22f0f6e9eba20e3f0e4e6dc10bad8
박명호 <qkraudghgh@naver.com>
2015-09-07 17:23:23 +0900

    로컬에서 작업하기위한 커

--- app/static/js/start.js
+++ app/static/js/start.js
@@ -2,13 +2,13 @@ angular.module('app')
     .controller('startCtrl', ['$scope','$http', '$rootScope', '$state', function($scope, $http, $rootScope, $state) {
         var url = "http://pleaselunch.tk"+Flask.url_for("StartView:getfoodlist");
 
-        $http.get(url)
+        $http.get('http://127.0.0.1:8080/getfoodlist/')
         .success(function(response){
             $scope.foodlist = response.foodlist;
         });
 
         $scope.sendrating = function(rating, foodName, userId){
-            $http.post(url, angular.fromJson({userId : userId, foodName : foodName, rating : rating}))
+            $http.post('http://127.0.0.1:8080/getfoodlist/', angular.fromJson({userId : userId, foodName : foodName, rating : rating}))
             alert(foodName + " is " +rating +"점 " + userId);
         }
 
