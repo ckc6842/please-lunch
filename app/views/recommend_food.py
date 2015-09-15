0cc90885058abbd619a68c989c8b3ca5cdb341eb
박명호 <qkraudghgh@naver.com>
2015-09-15 21:39:01 +0900

    get_res함수 지움, 일단 프로토 타입으로 단맛, 짠맛, 매운맛만 검사해서 최종 10개의 리스트를 뽑은다음에 랜덤으로 음식 하나를 골라줌

--- app/views/recommend_food.py
+++ app/views/recommend_food.py
@@ -1,19 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'bagmyeongho'
 
-from collections import defaultdict
-from operator import itemgetter
+import random
 from app.models import FoodScore, UserFoodScore, UserScore, User
 from app import db
-
-
-# 딕셔너리 val값을 비교하여 최소인 key값을 출력해주는 함수
-def get_res(dVals):
-    res = defaultdict(list)
-    for k, v in dVals.items():
-        print k,v
-        res[v].append(k)
-    return min(res.items(), key=itemgetter(0))[1]
 
 
 def recommend_food(user):
@@ -24,7 +14,6 @@ def recommend_food(user):
     sum_sweety = []
     sum_spicy = []
 
-    print db.session.query(UserFoodScore, FoodScore).filter_by(user_id=user.id).all()
     # temp에 접속해 있는 유저가 평가한 음식의 점수를 모두 불러온다 user_id에는 나중에 현재 접속한 유저의 값이 들어오도록 수정
     user_food_score_by_id = UserFoodScore.query.filter_by(user_id = user.id).all()
 
@@ -43,7 +32,7 @@ def recommend_food(user):
     object_temp = UserScore(2, "Taste", 1, round(user_salty, 2))  # 짠맛 targetId = 1
     db.session.add(object_temp)
 
-    print round(user_salty, 2)
+    # print round(user_salty, 2)
 
     # 단맛
     for v in user_food_score_by_id:
@@ -53,7 +42,7 @@ def recommend_food(user):
 
     user_sweety = (sum(sum_sweety) / (max_rating * max_star * float(len(sum_sweety))))*4+1
 
-    print round(user_sweety, 2)
+    # print round(user_sweety, 2)
     object_temp = UserScore(2, "Taste", 2, round(user_sweety, 2)) # 단맛 targetId = 2
     db.session.add(object_temp)
 
@@ -65,7 +54,7 @@ def recommend_food(user):
 
     user_spicy = (sum(sum_spicy) / (max_rating * max_star * float(len(sum_spicy))))*4+1
 
-    print round(user_spicy, 2)
+    # print round(user_spicy, 2)
     object_temp = UserScore(2, "Taste", 3, round(user_spicy, 2)) # 매운맛 targetId = 3
     db.session.add(object_temp)
     db.session.commit()
@@ -82,7 +71,6 @@ def recommend_food(user):
             if v.targetId == 1:
                 sim = abs(v.score - user_salty)
             elif v.targetId == 2:
-                sim = abs(v.score - user_salty)
                 sim = abs(v.score - user_sweety)
             elif v.targetId == 3:
                 sim = abs(v.score - user_spicy)
@@ -96,8 +84,12 @@ def recommend_food(user):
                 sim = abs(v.score - user_spicy)
             temp_dic[temp_name] = round(sim, 2)
 
-    # print get_res(score_list) # get_res라는 함수를 이용하여 score_list에 들어있는 가장 최소값의 value값을 가진  Key값을 출력한다.
-    return get_res(temp_dic)[0].decode('utf-8')
+    #final_list엔 최종적으로 맛의 유사도가 높은 것들 10개를 선정하여 저장한다.
+    final_list= []
 
-if __name__ == '__main__':
-    print recommend_food(User.query.filter_by(email='maxtortime@gmail.com').one())
+    #temp_dic에 있는 모든 음식과 그 음식의 유사도에서 value값이 최소인 것들을 차례대로 pop하여 final_list에 넣는다.
+    for v in range(0, 10):
+        final_list.append(min(temp_dic, key=temp_dic.get))
+        temp_dic.pop(min(temp_dic, key=temp_dic.get), None)
+    #일단 DB에 넣는 과정은 생략함
+    return final_list[random.randrange(0,10)]
