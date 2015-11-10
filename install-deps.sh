764681d802826d25a081b2ba02dd840bd0dd8a1f
Jaewon Choi <jainersoer@ajou.ac.kr>
2015-11-10 19:48:48 +0900

    Added install-deps.
    This commit includes several dependency of this projects.

---
+++ install-deps.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Assume that you are using virtualenv or pyvenv.
+pip install Flask flask_sqlalchemy flask_triangle flask_mail flask_babel flask-login flask-security flask_jsglue celery flask_social_blueprint flask_classy pymysql
